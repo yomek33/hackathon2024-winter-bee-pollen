052b278caa04b220626b2841c917f87422742b64
chiyoho <chiyoho@chiyohonoMacBook-Pro.local>
2024-02-29 15:20:42 +0900

    HTML/CSSの追加

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import HomeView from "./views/HomeView.vue";
 </script>
 
 <template>
   <header></header>
-
+  <HomeView />>
   <RouterView />
 </template>
 
