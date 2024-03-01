ef68a19a0fdf093fedb3ed6cf3e8829c32fa4452
yomek33 <i3landme@gmail.com>
2024-03-01 10:54:00 +0900

    不要インポート消す

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
 import { RouterView } from "vue-router";
 import HomeView from "./views/HomeView.vue";
-import japanmap from "./components/japanmap.vue";
 import PrCard from "@/components/PrCard.vue";
 </script>
 
 <template>
   <header></header>
   <HomeView />
-  <!-- <japanmap /> -->
   <!-- <PrCard /> -->
   <RouterView />
 </template>
